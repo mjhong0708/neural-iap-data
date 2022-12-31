c170f4feb414dd2f786867f304be6074b0e3e558
Minjoon Hong <mjhong0708@yonsei.ac.kr>
2022-12-31 19:43:30 +0900

    add test for n_atoms and volume

--- tests/commons.py
+++ tests/commons.py
@@ -1,21 +1,16 @@
 import ase.build
-import numpy as np
-from ase.calculators.singlepoint import SinglePointCalculator
+from ase.calculators.emt import EMT
 
 
 def get_test_molecule():
     molecule = ase.build.molecule("C6H6")
-    energy = 0.0
-    forces = np.zeros_like(molecule.positions)
-    calc = SinglePointCalculator(molecule, energy=energy, forces=forces)
+    calc = EMT()
     molecule.calc = calc
     return molecule
 
 
 def get_test_bulk(repeat=(1, 1, 1)):
     bulk = ase.build.bulk("Pt", "fcc", a=3.9, cubic=True).repeat(repeat)
-    energy = 0.0
-    forces = np.zeros_like(bulk.positions)
-    calc = SinglePointCalculator(bulk, energy=energy, forces=forces)
+    calc = EMT()
     bulk.calc = calc
     return bulk
--- tests/test_atoms_graph.py
+++ tests/test_atoms_graph.py
@@ -1,5 +1,6 @@
 import torch
-from commons import get_test_molecule
+from commons import get_test_bulk, get_test_molecule
+from torch_geometric.data import Batch
 
 from neural_iap_data import AtomsGraph
 
@@ -21,3 +22,27 @@ def test_atoms_graph_neighbor():
     neighbor_idx, center_idx = atoms_graph.edge_index
     for i in range(len(atoms)):
         assert len(neighbor_idx[center_idx == i]) == 11
+
+
+def test_atoms_graph_n_atoms():
+    atoms_1 = get_test_bulk()
+    atoms_2 = atoms_1.repeat((2, 2, 2))
+    atoms_2.calc = atoms_1.calc
+    g1 = AtomsGraph.from_ase(atoms_1)
+    g2 = AtomsGraph.from_ase(atoms_2)
+    batch = Batch.from_data_list([g1, g2])
+    n_atoms = batch.n_atoms()
+    assert n_atoms[0] == len(atoms_1)
+    assert n_atoms[1] == len(atoms_2)
+
+
+def test_atoms_graph_volume():
+    atoms_1 = get_test_bulk()
+    atoms_2 = atoms_1.repeat((2, 2, 2))
+    atoms_2.calc = atoms_1.calc
+    g1 = AtomsGraph.from_ase(atoms_1)
+    g2 = AtomsGraph.from_ase(atoms_2)
+    batch = Batch.from_data_list([g1, g2])
+    volume = batch.volume()
+    assert torch.allclose(volume[0], torch.as_tensor(atoms_1.get_volume(), dtype=torch.float))
+    assert torch.allclose(volume[1], torch.as_tensor(atoms_2.get_volume(), dtype=torch.float))
